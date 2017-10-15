3303d86ee80b2ee640f543453b55bf01c25fadc4
Muchiachio <aldis.rameika@gmail.com>
2017-10-15 15:27:27 +0300

    Consistent datalist dialog width
    - Fixed, datalist dialog not recentering itself correctly when new
    content size exceeeds free space on the right, resulting in centering of
    not yet fully extended dialog window.

--- src/Datalist/Scripts/MvcDatalist/mvc-datalist.js
+++ src/Datalist/Scripts/MvcDatalist/mvc-datalist.js
@@ -164,6 +164,7 @@ var MvcDatalistDialog = (function () {
                 this.renderBody(data.Columns, data.Rows);
                 this.renderFooter(data.FilteredRows);
 
+                this.instance.parent().position({ my: 'left top', at: 'left top+50px', of: window, within: window });
                 this.instance.dialog({
                     position: this.options.dialog.position || { my: 'center top', at: 'center top+50px', of: window, within: window }
                 });
